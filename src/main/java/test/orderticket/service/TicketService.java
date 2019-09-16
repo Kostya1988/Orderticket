fab30bb90e7e8b20f070270b7288a6ec3a7f0a56
Kostya1988 <46385286+Kostya1988@users.noreply.github.com>
2019-09-16 23:03:46 +0300

    Update TicketService.java

--- src/main/java/test/orderticket/service/TicketService.java
+++ src/main/java/test/orderticket/service/TicketService.java
@@ -3,15 +3,11 @@ package test.orderticket.service;
 
 import test.orderticket.model.ReturnReference;
 import test.orderticket.model.TicketEntity;
-
 import java.util.List;
 
 
 public interface TicketService {
-
    void save(TicketEntity ticket);
-
    List<TicketEntity> orderStatus(String orderStatus);
    TicketEntity findByReference(String routeNumber);
-
 }
